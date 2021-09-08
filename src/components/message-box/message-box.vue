834c4ece4119479109ce2493be6fce70a595d4ff
wangbin <316281400@qq.com>
2021-09-08 16:31:39 +0800

    更新组件修复部分bug

--- src/components/message-box/message-box.vue
+++ src/components/message-box/message-box.vue
@@ -1,5 +1,5 @@
 <template>
-  <transition name="fade-in-linear" @after-leave="$emit('vanish')">
+  <transition name="modal-fade" @after-leave="$emit('vanish')">
     <b-mask
       v-show="visible"
       :z-index="state.zIndex"
@@ -7,51 +7,54 @@
       :mask="mask"
       @click.self="handleWrapperClick"
     >
-      <div
-        ref="root"
-        :aria-label="title || 'dialog'"
-        aria-modal="true"
-        :class="[ 'bin-message-box',customClass, ]"
-      >
+      <transition name="dialog-fade">
         <div
-          v-if="title !== null && title !== undefined"
-          class="bin-message-box__header"
+          v-show="visible"
+          ref="modalRef"
+          :aria-label="title || 'dialog'"
+          aria-modal="true"
+          :class="[ 'bin-message-box',customClass, ]"
         >
-          <i v-if="icon"
-             :class="['bin-message-box__status',`b-iconfont b-icon-${icon} is-${type}` ]"
-          ></i>
-          <div class="bin-message-box__title">
-            <span>{{ title }}</span>
+          <div
+            v-if="title !== null && title !== undefined"
+            class="bin-message-box__header"
+          >
+            <i v-if="icon"
+               :class="['bin-message-box__status',`b-iconfont b-icon-${icon} is-${type}` ]"
+            ></i>
+            <div class="bin-message-box__title">
+              <span>{{ title }}</span>
+            </div>
+          </div>
+          <div v-if="hasMessage" class="bin-message-box__message">
+            <slot>
+              <p v-if="!useHTML">{{ message }}</p>
+              <p v-else v-html="message"></p>
+            </slot>
+          </div>
+          <div class="bin-message-box__footer">
+            <b-button
+              v-if="showCancelButton"
+              :loading="state.cancelButtonLoading"
+              :class="[cancelButtonClass]"
+              @click="handleAction('cancel')"
+            >
+              {{ state.cancelText }}
+            </b-button>
+            <b-button
+              v-show="showConfirmButton"
+              ref="confirmRef"
+              :loading="state.confirmButtonLoading"
+              :class="[confirmButtonClasses]"
+              type="primary"
+              :disabled="state.confirmButtonDisabled"
+              @click="handleAction('confirm')"
+            >
+              {{ state.confirmText }}
+            </b-button>
           </div>
         </div>
-        <div v-if="hasMessage" class="bin-message-box__message">
-          <slot>
-            <p v-if="!useHTML">{{ message }}</p>
-            <p v-else v-html="message"></p>
-          </slot>
-        </div>
-        <div class="bin-message-box__footer">
-          <b-button
-            v-if="showCancelButton"
-            :loading="state.cancelButtonLoading"
-            :class="[cancelButtonClass]"
-            @click="handleAction('cancel')"
-          >
-            {{ state.cancelText }}
-          </b-button>
-          <b-button
-            v-show="showConfirmButton"
-            ref="confirmRef"
-            :loading="state.confirmButtonLoading"
-            :class="[confirmButtonClasses]"
-            type="primary"
-            :disabled="state.confirmButtonDisabled"
-            @click="handleAction('confirm')"
-          >
-            {{ state.confirmText }}
-          </b-button>
-        </div>
-      </div>
+      </transition>
     </b-mask>
   </transition>
 </template>
@@ -71,6 +74,58 @@ import BMask from '../modal/mask.vue'
 import { useModal, useLockScreen, useRestoreActive } from '../../hooks'
 import { transferIncrease } from '../../utils/config'
 import { on, off } from '../../utils/dom'
+import { addEventListenerWrap } from '../modal/addListener'
+
+let mousePosition = null
+const getClickPosition = (e) => {
+  mousePosition = {
+    x: e.pageX,
+    y: e.pageY,
+  }
+  // 100ms 内发生过点击事件，则从点击位置动画展示
+  // 否则直接 zoom 展示
+  // 这样可以兼容非点击方式展开
+  setTimeout(() => (mousePosition = null), 100)
+}
+
+// 只有点击事件支持从鼠标位置动画展开
+if (typeof window !== 'undefined' && window.document && window.document.documentElement) {
+  addEventListenerWrap(document.documentElement, 'click', getClickPosition, true)
+}
+
+function getScroll(w, top) {
+  let ret = w[`page${top ? 'Y' : 'X'}Offset`]
+  const method = `scroll${top ? 'Top' : 'Left'}`
+  if (typeof ret !== 'number') {
+    const d = w.document
+    ret = d.documentElement[method]
+    if (typeof ret !== 'number') {
+      ret = d.body[method]
+    }
+  }
+  return ret
+}
+
+function setTransformOrigin(node, value) {
+  const style = node.style;
+  ['Webkit', 'Moz', 'Ms', 'ms'].forEach(prefix => {
+    style[`${prefix}TransformOrigin`] = value
+  })
+  style[`transformOrigin`] = value
+}
+
+function offset(el) {
+  const rect = el.getBoundingClientRect()
+  const pos = {
+    left: rect.left,
+    top: rect.top,
+  }
+  const doc = el.ownerDocument
+  const w = doc.defaultView || doc.parentWindow
+  pos.left += getScroll(w, false)
+  pos.top += getScroll(w, true)
+  return pos
+}
 
 const TypeMap = {
   'info': 'info-circle',
@@ -145,6 +200,7 @@ export default {
   },
   emits: ['vanish', 'action'],
   setup(props, { emit }) {
+    const modalRef = ref(null)
     // const popup = usePopup(props, doClose)
     const visible = ref(false)
     // s represents state
@@ -163,6 +219,23 @@ export default {
 
     const confirmButtonClasses = computed(() => `b-button--primary ${props.confirmButtonClass}`)
 
+    const updateCallback = (flag) => {
+      if (visible.value) {
+        // first show
+        if (!flag) {
+          const dialogNode = modalRef.value
+          if (mousePosition) {
+            const elOffset = offset(dialogNode)
+            setTransformOrigin(
+              dialogNode,
+              `${mousePosition.x - elOffset.left}px ${mousePosition.y - elOffset.top}px`,
+            )
+          } else {
+            setTransformOrigin(dialogNode, '')
+          }
+        }
+      }
+    }
     watch(() => visible.value, val => {
       if (val) {
         if (props.type === 'alert' || props.type === 'confirm') {
@@ -172,6 +245,9 @@ export default {
         }
         state.zIndex = transferIncrease()
       }
+      nextTick(() => {
+        updateCallback(!val)
+      })
     })
 
     onMounted(async () => {
@@ -231,6 +307,7 @@ export default {
     useRestoreActive(visible)
 
     return {
+      modalRef,
       state,
       visible,
       hasMessage,
